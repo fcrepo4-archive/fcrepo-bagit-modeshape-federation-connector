69c409fb52c16501d7e446b2be9bcd007f92cd2b
Esme Cowles <escowles@ucsd.edu>
2013-04-23 16:21:57 -0400

    Adding test of moving new bag into federated directory

--- src/test/java/org/fcrepo/federation/bagit/BagItConnectorIT.java
+++ src/test/java/org/fcrepo/federation/bagit/BagItConnectorIT.java
@@ -6,7 +6,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 
 import javax.inject.Inject;
 import javax.jcr.Node;
@@ -25,17 +29,26 @@ import org.fcrepo.jaxb.responses.access.ObjectProfile;
 import org.fcrepo.services.PathService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.modeshape.jcr.JcrSession;
 import org.modeshape.jcr.api.Session;
+import org.modeshape.jcr.api.Workspace;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import gov.loc.repository.bagit.Bag;
+import gov.loc.repository.bagit.BagFactory;
+import gov.loc.repository.bagit.PreBag;
+import gov.loc.repository.bagit.writer.impl.FileSystemWriter;
+import gov.loc.repository.bagit.transformer.impl.DefaultCompleter;
+
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/spring-test/master.xml")
 public class BagItConnectorIT {
 	private static Logger logger = LoggerFactory.getLogger(BagItConnectorIT.class);
-	
+
     @Inject
     Repository repo;
 
@@ -67,7 +80,84 @@ public class BagItConnectorIT {
 //                uriInfo.getAbsolutePathBuilder().path("datastreams").build();
         objectProfile.objState = A;
         objectProfile.objModels = obj.getModels();
-		
 	}
 
+	@Test
+	public void tryFilesystemUpdates() throws IOException, RepositoryException {
+		JcrSession session = (JcrSession)repo.login();
+
+		// create a random bag and move it into the federated directory
+		File baseDir = new File( "./target/test-classes" );
+        File srcDir = new File( baseDir, "tmp-objects" );
+		File dstDir = new File( baseDir, "test-objects" );
+        makeRandomBags( srcDir, 1, 1, 1024L );
+		File srcBag = new File( srcDir, "randomBag0" );
+		File dstBag = new File( dstDir, "randomBag0" );
+		srcBag.renameTo( dstBag );
+
+		// check that the bag shows up in the federation
+		Node node = session.getNode("/objects/randomBag0");
+		logger.info("Got node at " + node.getPath());
+		PropertyIterator properties = node.getProperties();
+		assertTrue(properties.hasNext());
+		Property property = node.getProperty("bagit:Bag.Size");
+		assertNotNull(property);
+		NodeIterator nodes = node.getNodes();
+		assertTrue("/objects/randomBag0 had no child nodes!", nodes.hasNext());
+		Node child = nodes.nextNode();
+		nodes = child.getNodes();
+		assertEquals("jcr:content", nodes.nextNode().getName());
+		FedoraObject obj = new FedoraObject(session, PathService.getObjectJcrNodePath("BagItFed1"));
+        final ObjectProfile objectProfile = new ObjectProfile();
+        objectProfile.pid = obj.getName();
+        objectProfile.objLabel = obj.getLabel();
+        objectProfile.objOwnerId = obj.getOwnerId();
+        objectProfile.objCreateDate = obj.getCreated();
+        objectProfile.objLastModDate = obj.getLastModified();
+        objectProfile.objSize = obj.getSize();
+        objectProfile.objState = A;
+        objectProfile.objModels = obj.getModels();
+	}
+	private static void copyFiles( File srcDir, File dstDir ) throws IOException
+	{
+		dstDir.mkdirs();
+		for ( File srcFile : srcDir.listFiles() )
+		{
+			if ( srcFile.isFile() )
+			{
+				File dstFile = new File( dstDir, srcFile.getName() );
+				logger.debug("copying: " + srcFile.getAbsolutePath() + " -> " + dstFile.getAbsolutePath());
+				Files.copy( srcFile.toPath(), dstFile.toPath() );
+			}
+		}
+	}
+	private static void makeRandomBags( File baseDir, int bagCount,
+		int fileCount, long fileSize ) throws IOException
+	{
+		BagFactory factory = new BagFactory();
+		DefaultCompleter completer = new DefaultCompleter(factory);
+		FileSystemWriter writer = new FileSystemWriter(factory);
+		for ( int i = 0; i < bagCount; i++ )
+		{
+			logger.debug("Creating random bag: " + i);
+			File bagDir = new File( baseDir, "randomBag" + i);
+			File dataDir = new File( bagDir, "data" );
+			if ( !dataDir.exists() )
+			{
+				dataDir.mkdirs();
+			}
+			for ( int j = 0; j < fileCount; j++ )
+			{
+				File dataFile = new File( dataDir, "randomFile" + j );
+				BufferedWriter buf = new BufferedWriter( new FileWriter(dataFile) );
+				for ( long k = 0L; k < fileSize; k++ )
+				{
+					buf.write( (int)(Math.random() * 10) );
+				}
+			}
+			PreBag pre = factory.createPreBag( bagDir );
+			Bag bag = pre.makeBagInPlace( BagFactory.LATEST, true, completer );
+			bag.write( writer, bagDir );
+		}
+	}
 }
